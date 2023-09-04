68fb4d3dd1e51ba96e38cc6b68165c334796b037
Skyness60 <sami.perron60@gmail.com>
2023-09-04 16:53:37 +0200

    add pagination

--- fonctions/article.fn.php
+++ fonctions/article.fn.php
@@ -40,15 +40,227 @@ $articles = [
         "image" => "https://previews.123rf.com/images/yuliiajd/yuliiajd1705/yuliiajd170500227/78106965-fond-de-for%C3%AAt-myst%C3%A9rieuse-printemps-avec-chemin-d-acc%C3%A8s-sur-une-journ%C3%A9e-ensoleill%C3%A9e.jpg",
         "created_at" => "12/06/2023",
         "enabled" => true
-    ]
+    ],
+    5 => [
+        "id" => 6,
+        "title" => "Photo au dessus d'une montagne",
+        "content" => "Cette photo a été prise en hommage à un grand alpiniste",
+        "image" => "https://static-cse.canva.com/blob/996500/Sanstitre.jpg",
+        "created_at" => "14/08/2023",
+        "enabled" => true
+    ], 
+    6 => [
+        "id" => 7,
+        "title" => "Photo d'une forêt mystérieuse",
+        "content" => "Explorez les secrets cachés de cette forêt enchanteresse",
+        "image" => "https://static-cse.canva.com/blob/123456/Foret.jpg",
+        "created_at" => "11/08/2023",
+        "enabled" => false
+    ], 
+    7 => [
+        "id" => 8,
+        "title" => "Aurore boréale magique",
+        "content" => "Laissez-vous émerveiller par le spectacle de l'aurore boréale dans le ciel nocturne",
+        "image" => "https://upload.wikimedia.org/wikipedia/commons/9/96/Aurore_bor%C3%A9ale_au_Saguenay%E2%88%92Lac-Saint-Jean.jpg",
+        "created_at" => "05/04/2023",
+        "enabled" => true
+    ],
+    8 => [
+        "id" => 9,
+        "title" => "Exploration urbaine",
+        "content" => "Découvrez les rues animées de la ville et ses recoins cachés",
+        "image" => "https://st.depositphotos.com/27201292/53543/i/450/depositphotos_535436052-stock-photo-an-old-abandoned-factory-in.jpg",
+        "created_at" => "01/02/2023",
+        "enabled" => true
+    ],
+    9 => [
+        "id" => 10,
+        "title" => "Aventure sous-marine",
+        "content" => "Plongez dans les profondeurs de l'océan pour découvrir sa faune incroyable",
+        "image" => "https://previews.123rf.com/images/yuliiajd/yuliiajd1705/yuliiajd170500227/78106965-fond-de-for%C3%AAt-myst%C3%A9rieuse-printemps-avec-chemin-d-acc%C3%A8s-sur-une-journ%C3%A9e-ensoleill%C3%A9e.jpg",
+        "created_at" => "12/06/2023",
+        "enabled" => true
+    ],
+
+    10 => [
+        "id" => 6,
+        "title" => "Photo au dessus d'une montagne",
+        "content" => "Cette photo a été prise en hommage à un grand alpiniste",
+        "image" => "https://static-cse.canva.com/blob/996500/Sanstitre.jpg",
+        "created_at" => "14/08/2023",
+        "enabled" => true
+    ], 
+    11 => [
+        "id" => 7,
+        "title" => "Photo d'une forêt mystérieuse",
+        "content" => "Explorez les secrets cachés de cette forêt enchanteresse",
+        "image" => "https://static-cse.canva.com/blob/123456/Foret.jpg",
+        "created_at" => "11/08/2023",
+        "enabled" => false
+    ], 
+    12 => [
+        "id" => 8,
+        "title" => "Aurore boréale magique",
+        "content" => "Laissez-vous émerveiller par le spectacle de l'aurore boréale dans le ciel nocturne",
+        "image" => "https://upload.wikimedia.org/wikipedia/commons/9/96/Aurore_bor%C3%A9ale_au_Saguenay%E2%88%92Lac-Saint-Jean.jpg",
+        "created_at" => "05/04/2023",
+        "enabled" => true
+    ],
+    13 => [
+        "id" => 9,
+        "title" => "Exploration urbaine",
+        "content" => "Découvrez les rues animées de la ville et ses recoins cachés",
+        "image" => "https://st.depositphotos.com/27201292/53543/i/450/depositphotos_535436052-stock-photo-an-old-abandoned-factory-in.jpg",
+        "created_at" => "01/02/2023",
+        "enabled" => true
+    ],
+    14 => [
+        "id" => 10,
+        "title" => "Aventure sous-marine",
+        "content" => "Plongez dans les profondeurs de l'océan pour découvrir sa faune incroyable",
+        "image" => "https://previews.123rf.com/images/yuliiajd/yuliiajd1705/yuliiajd170500227/78106965-fond-de-for%C3%AAt-myst%C3%A9rieuse-printemps-avec-chemin-d-acc%C3%A8s-sur-une-journ%C3%A9e-ensoleill%C3%A9e.jpg",
+        "created_at" => "12/06/2023",
+        "enabled" => true
+    ],
+
+    15 => [
+        "id" => 6,
+        "title" => "Photo au dessus d'une montagne",
+        "content" => "Cette photo a été prise en hommage à un grand alpiniste",
+        "image" => "https://static-cse.canva.com/blob/996500/Sanstitre.jpg",
+        "created_at" => "14/08/2023",
+        "enabled" => true
+    ], 
+    16 => [
+        "id" => 7,
+        "title" => "Photo d'une forêt mystérieuse",
+        "content" => "Explorez les secrets cachés de cette forêt enchanteresse",
+        "image" => "https://static-cse.canva.com/blob/123456/Foret.jpg",
+        "created_at" => "11/08/2023",
+        "enabled" => false
+    ], 
+    17 => [
+        "id" => 8,
+        "title" => "Aurore boréale magique",
+        "content" => "Laissez-vous émerveiller par le spectacle de l'aurore boréale dans le ciel nocturne",
+        "image" => "https://upload.wikimedia.org/wikipedia/commons/9/96/Aurore_bor%C3%A9ale_au_Saguenay%E2%88%92Lac-Saint-Jean.jpg",
+        "created_at" => "05/04/2023",
+        "enabled" => true
+    ],
+    18 => [
+        "id" => 9,
+        "title" => "Exploration urbaine",
+        "content" => "Découvrez les rues animées de la ville et ses recoins cachés",
+        "image" => "https://st.depositphotos.com/27201292/53543/i/450/depositphotos_535436052-stock-photo-an-old-abandoned-factory-in.jpg",
+        "created_at" => "01/02/2023",
+        "enabled" => true
+    ],
+    19 => [
+        "id" => 10,
+        "title" => "Aventure sous-marine",
+        "content" => "Plongez dans les profondeurs de l'océan pour découvrir sa faune incroyable",
+        "image" => "https://previews.123rf.com/images/yuliiajd/yuliiajd1705/yuliiajd170500227/78106965-fond-de-for%C3%AAt-myst%C3%A9rieuse-printemps-avec-chemin-d-acc%C3%A8s-sur-une-journ%C3%A9e-ensoleill%C3%A9e.jpg",
+        "created_at" => "12/06/2023",
+        "enabled" => true
+    ],
+
+    20 => [
+        "id" => 6,
+        "title" => "Photo au dessus d'une montagne",
+        "content" => "Cette photo a été prise en hommage à un grand alpiniste",
+        "image" => "https://static-cse.canva.com/blob/996500/Sanstitre.jpg",
+        "created_at" => "14/08/2023",
+        "enabled" => true
+    ], 
+    21 => [
+        "id" => 7,
+        "title" => "Photo d'une forêt mystérieuse",
+        "content" => "Explorez les secrets cachés de cette forêt enchanteresse",
+        "image" => "https://static-cse.canva.com/blob/123456/Foret.jpg",
+        "created_at" => "11/08/2023",
+        "enabled" => false
+    ], 
+    22 => [
+        "id" => 8,
+        "title" => "Aurore boréale magique",
+        "content" => "Laissez-vous émerveiller par le spectacle de l'aurore boréale dans le ciel nocturne",
+        "image" => "https://upload.wikimedia.org/wikipedia/commons/9/96/Aurore_bor%C3%A9ale_au_Saguenay%E2%88%92Lac-Saint-Jean.jpg",
+        "created_at" => "05/04/2023",
+        "enabled" => true
+    ],
+    23 => [
+        "id" => 9,
+        "title" => "Exploration urbaine",
+        "content" => "Découvrez les rues animées de la ville et ses recoins cachés",
+        "image" => "https://st.depositphotos.com/27201292/53543/i/450/depositphotos_535436052-stock-photo-an-old-abandoned-factory-in.jpg",
+        "created_at" => "01/02/2023",
+        "enabled" => true
+    ],
+    24 => [
+        "id" => 10,
+        "title" => "Aventure sous-marine",
+        "content" => "Plongez dans les profondeurs de l'océan pour découvrir sa faune incroyable",
+        "image" => "https://previews.123rf.com/images/yuliiajd/yuliiajd1705/yuliiajd170500227/78106965-fond-de-for%C3%AAt-myst%C3%A9rieuse-printemps-avec-chemin-d-acc%C3%A8s-sur-une-journ%C3%A9e-ensoleill%C3%A9e.jpg",
+        "created_at" => "12/06/2023",
+        "enabled" => true
+    ],
+
+    25 => [
+        "id" => 6,
+        "title" => "Photo au dessus d'une montagne",
+        "content" => "Cette photo a été prise en hommage à un grand alpiniste",
+        "image" => "https://static-cse.canva.com/blob/996500/Sanstitre.jpg",
+        "created_at" => "14/08/2023",
+        "enabled" => true
+    ], 
+    26 => [
+        "id" => 7,
+        "title" => "Photo d'une forêt mystérieuse",
+        "content" => "Explorez les secrets cachés de cette forêt enchanteresse",
+        "image" => "https://static-cse.canva.com/blob/123456/Foret.jpg",
+        "created_at" => "11/08/2023",
+        "enabled" => false
+    ], 
+    27 => [
+        "id" => 8,
+        "title" => "Aurore boréale magique",
+        "content" => "Laissez-vous émerveiller par le spectacle de l'aurore boréale dans le ciel nocturne",
+        "image" => "https://upload.wikimedia.org/wikipedia/commons/9/96/Aurore_bor%C3%A9ale_au_Saguenay%E2%88%92Lac-Saint-Jean.jpg",
+        "created_at" => "05/04/2023",
+        "enabled" => true
+    ],
+    28 => [
+        "id" => 9,
+        "title" => "Exploration urbaine",
+        "content" => "Découvrez les rues animées de la ville et ses recoins cachés",
+        "image" => "https://st.depositphotos.com/27201292/53543/i/450/depositphotos_535436052-stock-photo-an-old-abandoned-factory-in.jpg",
+        "created_at" => "01/02/2023",
+        "enabled" => true
+    ],
+    29 => [
+        "id" => 10,
+        "title" => "Aventure sous-marine",
+        "content" => "Plongez dans les profondeurs de l'océan pour découvrir sa faune incroyable",
+        "image" => "https://previews.123rf.com/images/yuliiajd/yuliiajd1705/yuliiajd170500227/78106965-fond-de-for%C3%AAt-myst%C3%A9rieuse-printemps-avec-chemin-d-acc%C3%A8s-sur-une-journ%C3%A9e-ensoleill%C3%A9e.jpg",
+        "created_at" => "12/06/2023",
+        "enabled" => true
+    ],
+    
 ];
 
-function getArticles() 
+function getEnabledArticles()
 {
     global $articles;
     return array_filter($articles, function ($article) {
         return $article['enabled'];
     });
+}
+
+
+function getArticles($page, $articlesPerPage) 
+{
+    $offset = ($page - 1) * $articlesPerPage;
+    return array_slice(getEnabledArticles(), $offset, $articlesPerPage);
 }
 
 function getArticle($id) 
@@ -57,5 +269,12 @@ function getArticle($id)
     $index = array_search($id, array_column($articles, 'id'));
     return $articles[$index];
 }
+
+function countArticles()
+{
+    return count(getEnabledArticles());
+}
+
+
 
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 
 require("_header.php");
-$articles = getArticles();
+$perPage = 5;
+$currentPage = (int) $_GET['page'];
+$total = countArticles();
+$lastPage = ceil($total / $perPage);
+$articles = getArticles($currentPage, $perPage);
 
 ?>
 
@@ -35,8 +39,23 @@ $articles = getArticles();
         </ul>
       </article>
     <?php endforeach; ?>
+
+    <ul>
+      <?php if ($currentPage > 1) : ?>
+        <li><a href="index.php?page=<?=($currentPage - 1) ?>">&leftarrow;</a></li>
+      <?php endif; ?>
+      <?php for ($i = 1; $i <= $lastPage; $i++) : ?>
+        <li><a href="index.php?page=<?=$i?>"><?=$i?></a></li>
+      <?php endfor; ?>
+      <?php if ($currentPage < $lastPage) : ?>
+        <li><a href="index.php?page=<?=($currentPage + 1) ?>">&rightarrow;</a></li>
+      <?php endif; ?>
+    </ul>
+
     </main>
     </div>
+
+
 
     <!-- Le pied de page -->
 
